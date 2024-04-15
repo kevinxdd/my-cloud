023a2a3c0b1526d111329aeb40208176c17de43a
xiedongdong <xiedongdong@axzo.cn>
2024-04-15 11:43:00 +0800

    first commit

---
+++ order/src/main/java/com/xdd/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.xdd.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrderApplication.class, args);
+  }
+
+}
